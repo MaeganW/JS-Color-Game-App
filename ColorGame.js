89b43ab5dcdcdfcafed5cb2ef2b620fd3c4ebce9
Maegan Womble <maeganwomble21@gmail.com>
2017-05-03 20:15:49 -0500

    Removed commented code

--- ColorGame.js
+++ ColorGame.js
@@ -6,8 +6,6 @@ var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.getElementById('message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
-//var easyBtn = document.querySelector('#easyBtn');
-//var hardBtn = document.querySelector('#hardBtn');
 var modeButtons = document.querySelectorAll('.mode');
 
 init();
@@ -26,15 +24,6 @@ function setupModeButtons() {
             modeButtons[1].classList.remove('selected');
             this.classList.add('selected');
             this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
-
-            //The above terniary operator is essentially this logic, but cleaner/refactored:
-
-            // if(this.textContent === "Easy"){
-            //     numSquares = 3;
-            // } else {
-            //     numSquares = 6;
-            // }       
-
             reset();
         });
     }
@@ -80,35 +69,6 @@ function reset() {
     messageDisplay.textContent = "";
     h1.style.backgroundColor = "steelblue";
 }
-
-//easyBtn.addEventListener('click', function () {
-//    easyBtn.classList.add('selected');
-//    hardBtn.classList.remove('selected');
-//    numSquares = 3;
-//    colors = generateRandomColors(numSquares);
-//    pickedColor = pickColor();
-//    colorDisplay.textContent = pickedColor;
-//    for (var i = 0; i < squares.length; i++) {
-//        if (colors[i]) {
-//            squares[i].style.background = colors[i];
-//        } else {
-//            squares[i].style.display = "none";
-//        }
-//    }
-//})
-//
-//hardBtn.addEventListener('click', function () {
-//    hardBtn.classList.add('selected');
-//    easyBtn.classList.remove('selected');
-//    numSqaures = 6;
-//    colors = generateRandomColors(numSquares);
-//    pickedColor = pickColor();
-//    colorDisplay.textContent = pickedColor;
-//    for (var i = 0; i < squares.length; i++) {
-//        squares[i].style.background = colors[i];
-//        squares[i].style.display = "block";
-//    }
-//});
 
 resetButton.addEventListener('click', function () {
     reset();
